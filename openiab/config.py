ca3884e96e6b14bcf359e5ee333e9f623adfdcbe
Vasiliy Makarov <drmoriarty.0@gmail.com>
2019-12-24 08:50:37 +0300

    update

--- openiab/config.py
+++ openiab/config.py
@@ -7,6 +7,6 @@ def configure(env):
 		env.android_add_maven_repository("url 'https://raw.githubusercontent.com/onepf/OPF-mvn-repo/master/'")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
 		env.android_add_to_permissions("android/AndroidManifestPermissionsChunk.xml")
-		env.android_add_dependency("compile 'org.onepf:openiab:0.9.8.4'")
+		env.android_add_dependency("compile 'org.onepf:openiab:0.9.8.6'")
 		env.android_add_dependency("compile 'com.amazon:in-app-purchasing:2.0.1'")
 		env.android_add_dependency("compile 'com.braintree:fortumo-in-app:9.1.2'")
